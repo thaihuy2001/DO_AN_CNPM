ed8a8558bd0f2dce17fc7463f666980f0b514c81
tohuythai <sajthaivip123@gmail.com>
2023-03-27 11:10:35 +0700

    thai them

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config['base_url'] = "http://localhost/do_an/";
+
+$config['default_module'] = 'home';
+$config['default_controller'] = 'index';
+$config['default_action'] = 'index';
+
+
+
+
+
+
+
+
+
+
+
+
